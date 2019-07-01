0a2f85a4bb405e7316482e2775dc6f65601a3672
ikerib <ikerib@gmail.com>
2019-07-01 10:52:56 +0200

    Kontratazioa CRUD

--- src/Controller/HutsakController.php
+++ src/Controller/HutsakController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/hutsak")
+ * @Route("/admin/hutsak")
  */
 class HutsakController extends AbstractController
 {

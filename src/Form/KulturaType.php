6941f872311d22c1185db04ebea999a181ffe96f
ikerib <ikerib@gmail.com>
2019-07-01 11:13:16 +0200

    Kultura saila

---
+++ src/Form/KulturaType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Kultura;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class KulturaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('espedientea')
+            ->add('data')
+            ->add('sailkapena')
+            ->add('signatura')
+            ->add('oharrak')
+            ->add('numdoc')
+            ->add('knosysid')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Kultura::class,
+        ]);
+    }
+}

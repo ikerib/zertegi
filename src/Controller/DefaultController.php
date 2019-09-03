47b292486e0521604816e152e00bb0409fce6b7c
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-03 21:30:07 +0200

    debug login

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,11 +4,58 @@ namespace App\Controller;
 
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/debug1", name="debug1", methods={"GET"})
+     * @param Request $request
+     */
+    public function debug1(Request $request)
+    {
+        new Response("OK");
+    }
+
+    /**
+     * @Route("/debug", name="debug", methods={"GET"})
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function debug(Request $request)
+    {
+        $user = $this->getUser();
+        $user = "iibarguren";
+        /**
+         * User objectu berria sortu, rol berriekin
+         */
+        $token = new UsernamePasswordToken(
+            $user,
+            null,
+            'main',
+            ["ROLE_ADMIN"]
+        );
+        $this->get( 'security.token_storage' )->setToken( $token );
+
+        /** @var Session $session */
+        $session = $request->getSession();
+
+        /* Localea zehaztu */
+
+        $request->setLocale('eu');
+
+
+
+        return $this->redirectToRoute('admin_home');
+    }
+
 
     /**
      * @Route("/admin/finder", name="admin_finder", methods={"GET"})

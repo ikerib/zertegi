25cee370273ff7fb9a6c3b98ac36b658414ad08b
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-04 19:22:02 +0200

    Ciriza

--- src/Controller/CirizaController.php
+++ src/Controller/CirizaController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Amp;
 use App\Entity\Ciriza;
 use App\Form\CirizaType;
 use App\Repository\CirizaRepository;
+use App\Service\DbHelperService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\QueryBuilder;
 use Knp\Component\Pager\PaginatorInterface;
@@ -18,31 +20,29 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/admin/ciriza")
  */
-class CirizaController extends AbstractController
-{
+class CirizaController extends AbstractController {
 
     /**
-     * @Route("/", name="ciriza_index", methods={"GET"})
+     * @Route("/", name="ciriza_index", methods={"GET", "POST"})
      * @param Request            $request
      * @param PaginatorInterface $paginator
      * @param CirizaRepository   $cirizaRepository
      *
      * @param SessionInterface   $session
      *
+     * @param DbHelperService    $dbhelper
+     *
      * @return Response
      */
-    public function index(Request $request, PaginatorInterface $paginator, CirizaRepository $cirizaRepository, SessionInterface $session): Response
-    {
-        /** @var QueryBuilder $queryBuilder */
-        $queryBuilder = $cirizaRepository->createQueryBuilder('a');
-
-        $filter = $request->query->get('filter');
-        if ($filter) {
-            $queryBuilder->where('MATCH_AGAINST(a.data, a.deskribapena, a.oharrak, a.sailkapena, a.signatura) AGAINST(:searchterm boolean)>0')
-                         ->setParameter('searchterm', $filter);
-        }
-
-        $query = $queryBuilder->getQuery();
+    public function index(
+        Request $request,
+        PaginatorInterface $paginator,
+        CirizaRepository $cirizaRepository,
+        SessionInterface $session,
+        DbHelperService $dbhelper
+    ): Response {
+        $myFilters = $dbhelper->getFinderParams($request->request->get('form'));
+        $query     = $cirizaRepository->getQueryByFinder($myFilters);
 
         $cirizas = $paginator->paginate(
             $query, /* query NOT result */
@@ -51,17 +51,24 @@ class CirizaController extends AbstractController
         );
 
         $myselection = $session->get('zertegi-selection');
-        if ($myselection !== null) {
+        if ($myselection !== null)
+        {
             if (array_key_exists('ciriza', $myselection))
             {
                 $myselection = $myselection[ 'ciriza' ];
             }
         }
 
-        return $this->render('ciriza/index.html.twig', [
-            'cirizas' => $cirizas,
-            'myselection' => $myselection
-        ]);
+        $fields = $dbhelper->getAllEntityFields(Ciriza::class);
+
+        return $this->render(
+            'ciriza/index.html.twig',
+            [
+                'cirizas'     => $cirizas,
+                'myselection' => $myselection,
+                'fields'      => $fields,
+            ]
+        );
     }
 
     /**
@@ -73,22 +80,27 @@ class CirizaController extends AbstractController
     public function new(Request $request): Response
     {
         $ciriza = new Ciriza();
-        $form = $this->createForm(CirizaType::class, $ciriza);
+        $form   = $this->createForm(CirizaType::class, $ciriza);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())
+        {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($ciriza);
             $entityManager->flush();
 
             $this->addFlash('success', 'Datuak ongi grabatu dira.');
+
             return $this->redirectToRoute('ciriza_index');
         }
 
-        return $this->render('ciriza/new.html.twig', [
-            'ciriza' => $ciriza,
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'ciriza/new.html.twig',
+            [
+                'ciriza' => $ciriza,
+                'form'   => $form->createView(),
+            ]
+        );
     }
 
     /**
@@ -99,9 +111,12 @@ class CirizaController extends AbstractController
      */
     public function show(Ciriza $ciriza): Response
     {
-        return $this->render('ciriza/show.html.twig', [
-            'ciriza' => $ciriza,
-        ]);
+        return $this->render(
+            'ciriza/show.html.twig',
+            [
+                'ciriza' => $ciriza,
+            ]
+        );
     }
 
     /**
@@ -116,19 +131,27 @@ class CirizaController extends AbstractController
         $form = $this->createForm(CirizaType::class, $ciriza);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())
+        {
             $this->getDoctrine()->getManager()->flush();
 
             $this->addFlash('success', 'Aldaketak ongi gorde dira.');
-            return $this->redirectToRoute('ciriza_index', [
-                'id' => $ciriza->getId(),
-            ]);
+
+            return $this->redirectToRoute(
+                'ciriza_index',
+                [
+                    'id' => $ciriza->getId(),
+                ]
+            );
         }
 
-        return $this->render('ciriza/edit.html.twig', [
-            'ciriza' => $ciriza,
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'ciriza/edit.html.twig',
+            [
+                'ciriza' => $ciriza,
+                'form'   => $form->createView(),
+            ]
+        );
     }
 
     /**
@@ -140,26 +163,32 @@ class CirizaController extends AbstractController
      */
     public function delete(Request $request, Ciriza $ciriza): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$ciriza->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$ciriza->getId(), $request->request->get('_token')))
+        {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($ciriza);
             $entityManager->flush();
-        } elseif ( $request->isXmlHttpRequest()) {
+        } elseif ($request->isXmlHttpRequest())
+        {
             $message = 'CSRF token error';
-            $resp = [
+            $resp    = [
                 'code' => 500,
-                'data' => $message
+                'data' => $message,
             ];
-            return new JsonResponse($resp,500);
-        } else {
+
+            return new JsonResponse($resp, 500);
+        } else
+        {
             return $this->redirectToRoute('ciriza_index');
         }
 
-        if ( $request->isXmlHttpRequest()) {
+        if ($request->isXmlHttpRequest())
+        {
             $resp = [
                 'code' => 200,
-                'data' => 'Ezabatua izan da'
+                'data' => 'Ezabatua izan da',
             ];
+
             return new JsonResponse($resp);
         }
 

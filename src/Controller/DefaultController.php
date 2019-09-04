b11c8df6b711bf82448e30d81a8706eaa5bdc9fd
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-04 18:36:56 +0200

    debug mode

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,11 +4,47 @@ namespace App\Controller;
 
 use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultController extends AbstractController
 {
+
+    /**
+     * @Route("/debug", name="admin_finder", methods={"GET"})
+     * @param Request               $request
+     * @param TokenStorageInterface $tokenStorage
+     *
+     * @return RedirectResponse
+     */
+    public function debug(Request $request, TokenStorageInterface $tokenStorage)
+    {
+
+        /**
+         * User objectu berria sortu, rol berriekin
+         */
+        $token = new UsernamePasswordToken(
+            "iibarguren",
+            null,
+            'main',
+            ['ROLE_ADMIN']
+        );
+
+        $tokenStorage->setToken($token);
+
+        /** @var Session $session */
+        $session = $request->getSession();
+
+        $request->setLocale('eu');
+
+
+        return $this->redirectToRoute('admin_home');
+    }
 
     /**
      * @Route("/admin/finder", name="admin_finder", methods={"GET"})

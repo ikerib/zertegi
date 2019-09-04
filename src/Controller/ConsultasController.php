5dcb2d48b36ba0538044dc4b3a3262ad22c812cb
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-09-04 19:26:12 +0200

    Consultas

--- src/Controller/ConsultasController.php
+++ src/Controller/ConsultasController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Amp;
 use App\Entity\Consultas;
 use App\Form\ConsultasType;
 use App\Repository\ConsultasRepository;
+use App\Service\DbHelperService;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\QueryBuilder;
 use Knp\Component\Pager\PaginatorInterface;
@@ -18,32 +20,29 @@ use Symfony\Component\Routing\Annotation\Route;
 /**
  * @Route("/admin/consultas")
  */
-class ConsultasController extends AbstractController
-{
+class ConsultasController extends AbstractController {
 
     /**
-     * @Route("/", name="consultas_index", methods={"GET"})
+     * @Route("/", name="consultas_index", methods={"GET", "POST"})
      * @param Request             $request
      * @param PaginatorInterface  $paginator
      * @param ConsultasRepository $consultasRepository
      *
      * @param SessionInterface    $session
      *
+     * @param DbHelperService     $dbhelper
+     *
      * @return Response
      */
-    public function index(Request $request, PaginatorInterface $paginator, ConsultasRepository $consultasRepository, SessionInterface $session): Response
-    {
-        /** @var QueryBuilder $queryBuilder */
-        $queryBuilder = $consultasRepository->createQueryBuilder('a');
-
-        $filter = $request->query->get('filter');
-        if ($filter) {
-            $queryBuilder->where('MATCH_AGAINST(a.enpresa, a.gaia, a.helbidea, a.izena, a.kontsulta) AGAINST(:searchterm boolean)>0')
-                         ->setParameter('searchterm', $filter);
-        }
-
-        $query = $queryBuilder->getQuery();
-
+    public function index(
+        Request $request,
+        PaginatorInterface $paginator,
+        ConsultasRepository $consultasRepository,
+        SessionInterface $session,
+        DbHelperService $dbhelper
+    ): Response {
+        $myFilters = $dbhelper->getFinderParams($request->request->get('form'));
+        $query     = $consultasRepository->getQueryByFinder($myFilters);
         $consultas = $paginator->paginate(
             $query, /* query NOT result */
             $request->query->getInt('page', 1)/*page number*/,
@@ -51,19 +50,22 @@ class ConsultasController extends AbstractController
         );
 
         $myselection = $session->get('zertegi-selection');
-        if ($myselection !== null) {
+        if ($myselection !== null)
+        {
             if (array_key_exists('consultas', $myselection))
             {
                 $myselection = $myselection[ 'consultas' ];
             }
         }
 
+        $fields = $dbhelper->getAllEntityFields(Consultas::class);
 
         return $this->render(
             'consultas/index.html.twig',
             [
-                'consultas' => $consultas,
-                'myselection' => $myselection
+                'consultas'   => $consultas,
+                'myselection' => $myselection,
+                'fields'      => $fields,
             ]
         );
     }
@@ -77,22 +79,27 @@ class ConsultasController extends AbstractController
     public function new(Request $request): Response
     {
         $consulta = new Consultas();
-        $form = $this->createForm(ConsultasType::class, $consulta);
+        $form     = $this->createForm(ConsultasType::class, $consulta);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())
+        {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($consulta);
             $entityManager->flush();
 
             $this->addFlash('success', 'Datuak ongi grabatu dira.');
+
             return $this->redirectToRoute('consultas_index');
         }
 
-        return $this->render('consultas/new.html.twig', [
-            'consulta' => $consulta,
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'consultas/new.html.twig',
+            [
+                'consulta' => $consulta,
+                'form'     => $form->createView(),
+            ]
+        );
     }
 
     /**
@@ -103,9 +110,12 @@ class ConsultasController extends AbstractController
      */
     public function show(Consultas $consulta): Response
     {
-        return $this->render('consultas/show.html.twig', [
-            'consulta' => $consulta,
-        ]);
+        return $this->render(
+            'consultas/show.html.twig',
+            [
+                'consulta' => $consulta,
+            ]
+        );
     }
 
     /**
@@ -120,19 +130,27 @@ class ConsultasController extends AbstractController
         $form = $this->createForm(ConsultasType::class, $consulta);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid())
+        {
             $this->getDoctrine()->getManager()->flush();
 
             $this->addFlash('success', 'Aldaketak ongi gorde dira.');
-            return $this->redirectToRoute('consultas_index', [
-                'id' => $consulta->getId(),
-            ]);
+
+            return $this->redirectToRoute(
+                'consultas_index',
+                [
+                    'id' => $consulta->getId(),
+                ]
+            );
         }
 
-        return $this->render('consultas/edit.html.twig', [
-            'consulta' => $consulta,
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'consultas/edit.html.twig',
+            [
+                'consulta' => $consulta,
+                'form'     => $form->createView(),
+            ]
+        );
     }
 
     /**
@@ -144,26 +162,32 @@ class ConsultasController extends AbstractController
      */
     public function delete(Request $request, Consultas $consulta): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$consulta->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$consulta->getId(), $request->request->get('_token')))
+        {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($consulta);
             $entityManager->flush();
-        } elseif ( $request->isXmlHttpRequest()) {
+        } elseif ($request->isXmlHttpRequest())
+        {
             $message = 'CSRF token error';
-            $resp = [
+            $resp    = [
                 'code' => 500,
-                'data' => $message
+                'data' => $message,
             ];
-            return new JsonResponse($resp,500);
-        } else {
+
+            return new JsonResponse($resp, 500);
+        } else
+        {
             return $this->redirectToRoute('consultas_index');
         }
 
-        if ( $request->isXmlHttpRequest()) {
+        if ($request->isXmlHttpRequest())
+        {
             $resp = [
                 'code' => 200,
-                'data' => 'Ezabatua izan da'
+                'data' => 'Ezabatua izan da',
             ];
+
             return new JsonResponse($resp);
         }
 
--- src/Repository/AmpRepository.php
+++ src/Repository/AmpRepository.php
@@ -24,11 +24,7 @@ class AmpRepository extends ServiceEntityRepository
         $qb = $this->createQueryBuilder('a');
 
         foreach ($arr as $key=>$value) {
-//            $sql = 'a.'.$key.' like :v'.$key;
-
             foreach ($value as $v) {
-//                $qb->orWhere($sql)
-//                   ->setParameter('v'.$key,$v);
                 $qb->orWhere(
                     $qb->expr()->like('a.'.$key,':v'.$key)
                 )->setParameter('v'.$key,'%'.$v.'%');
@@ -38,33 +34,4 @@ class AmpRepository extends ServiceEntityRepository
         return $qb->getQuery();
 
     }
-
-    // /**
-    //  * @return Amp[] Returns an array of Amp objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Amp
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }

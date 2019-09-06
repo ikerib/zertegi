14dca7817412f4760282c691f98fb343b2e0ff8b
ikerib <ikerib@gmail.com>
2019-09-06 07:39:32 +0200

    finder working

--- src/Repository/AmpRepository.php
+++ src/Repository/AmpRepository.php
@@ -6,6 +6,7 @@ use App\Entity\Amp;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Symfony\Bridge\Doctrine\RegistryInterface;
+use function Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Amp|null find($id, $lockMode = null, $lockVersion = null)
@@ -24,16 +25,36 @@ class AmpRepository extends ServiceEntityRepository
     public function getQueryByFinder($arr, $fields): Query
     {
         $qb = $this->createQueryBuilder('a');
+        $expr = $qb->expr();
         foreach ($arr as $key => $value) {
             $miindex = 0;
 
             if ( $key === 'Kontsulta') {
                 foreach ($fields as $field) {
+                    $contLoop=0;
                     foreach ($value as $v) {
+                        $toFind = [' ',',','?'];
+                        $clean = str_replace($toFind, '_', $v);
+
                         $qb->orWhere(
-                            $qb->expr()->like('a.'.$field, ':v'.$field.$miindex))->setParameter('v'.$field.$miindex, '%'.$v.'%');
+                            $qb->expr()->andX(
+                                $qb->expr()->andX($expr->like('a.'.$field, ':v'.$field.$miindex)),
+                                $qb->expr()->andX($expr->like('a.'.$field, ':v2'.$field.$miindex))
+                            )
+                        )->setParameter('v'.$field.$miindex, '%'.$v.'%')
+                        ->setParameter('v2'.$field.$miindex, '%'.$clean.'%');
+//                        $qb
+//                            ->orWhere(
+//                                $expr->andX(
+//                                    $expr->like('a.'.$field, ':v'.$field.$miindex),
+//                                    $expr->like('a.'.$field, ':v2'.$field.$miindex)
+//                                )
+//                            )->setParameter('v'.$field.$miindex, '%'.$v.'%')
+//                            ->setParameter('v2'.$field.$miindex, '%'.$clean.'%');
+
                         ++$miindex;
                     }
+                    ++$contLoop;
                 }
             } else {
                 foreach ($value as $v) {

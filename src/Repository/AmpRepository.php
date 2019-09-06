893b58dd7bc247a4fe51711fcfed0021dc31d1da
ikerib <ikerib@gmail.com>
2019-09-06 14:36:04 +0200

    Lanean

--- src/Repository/AmpRepository.php
+++ src/Repository/AmpRepository.php
@@ -6,7 +6,7 @@ use App\Entity\Amp;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use function Doctrine\ORM\QueryBuilder;
+
 
 /**
  * @method Amp|null find($id, $lockMode = null, $lockVersion = null)
@@ -28,41 +28,29 @@ class AmpRepository extends ServiceEntityRepository
         $expr = $qb->expr();
         foreach ($arr as $key => $value) {
             $miindex = 0;
-
             if ( $key === 'Kontsulta') {
                 foreach ($fields as $field) {
-                    $contLoop=0;
                     foreach ($value as $v) {
                         $toFind = [' ',',','?'];
                         $clean = str_replace($toFind, '__', $v);
 
                         $qb->andWhere(
                             $qb->expr()->orX(
-                                $qb->expr()->orX($expr->like('a.'.$field, ':v'.$field.$miindex)),
-                                $qb->expr()->orX($expr->like('a.'.$field, ':v2'.$field.$miindex))
+                                $expr->like($field, '%'.$v.'%'),
+                                $expr->like($field, '%'.$clean.'%')
                             )
-                        )->setParameter('v'.$field.$miindex, '%'.$v.'%')
-                        ->setParameter('v2'.$field.$miindex, '%'.$clean.'%');
-//                        $qb
-//                            ->orWhere(
-//                                $expr->andX(
-//                                    $expr->like('a.'.$field, ':v'.$field.$miindex),
-//                                    $expr->like('a.'.$field, ':v2'.$field.$miindex)
-//                                )
-//                            )->setParameter('v'.$field.$miindex, '%'.$v.'%')
-//                            ->setParameter('v2'.$field.$miindex, '%'.$clean.'%');
-
-                        ++$miindex;
+                        )
+                        ;
                     }
-                    ++$contLoop;
                 }
             } else {
                 foreach ($value as $v) {
-                    $qb->orWhere($qb->expr()->like('a.'.$key, ':v'.$key.$miindex))->setParameter('v'.$key.$miindex, '%'.$v.'%');
+                    $qb->andWhere($qb->expr()->like('a.'.$key, ':v'.$key.$miindex))->setParameter('v'.$key.$miindex, '%'.$v.'%');
                     ++$miindex;
                 }
             }
         }
+
 
         return $qb->getQuery();
     }
